82b436e8efcf765695975cf6bf01246ea4dd0d44
Korex23 <ololokes@gmail.com>
2023-08-29 14:23:35 +0100

    not a commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import DashboardListCard from "./components/Cards/DashboardCards";
 
 export default function App() {
-  return (
-    <div className="flex items-center mt-6 mx-28">
-      <DashboardListCard />
-    </div>
-  )
+  return;
 }
--- src/components/Cards/DashboardCards.jsx
+++ src/components/Cards/DashboardCards.jsx
@@ -1,50 +1,266 @@
-import React from 'react';
-import DashBoardImg from '../../assests/images/dashboard.png';
-import SettingsImg from '../../assests/images/settings.png';
-import styles from "../../styles/componentStyles/_dashboardlistcard.scss";
+import React from "react";
+import DashBoardImg from "../../assests/images/dashboard.png";
+import SettingsImg from "../../assests/images/settings.png";
+// import styles from "../../styles/componentStyles/_dashboardlistcard.scss";
 
-
-const DashboardListCard = () => {
-    return (
-        <div className=' py-8 px-7  flex  gap-8 bg-[#242222] rounded-xl relative'>
-            <img src={DashBoardImg} alt='dashboard' className='w-[368px] h-[300px] rounded-md border border-[#FFD21D]' />
-            <img src={SettingsImg} alt='settings' className='absolute top-8 right-8  cursor-pointer' />
-            <div>
-                <div className='flex items-center gap gap-2 mb-6'>
-                    <h2 className='text-[39px] font-bold font-sans text-white'>Okay Bears</h2>
-                    <div className='p-2'>
-                        <p className='font-sans text-[13px] text-[#FFD21D] bg-[rgba(218, 165, 33, 0.05) 100%]'>Brand</p>
-                    </div>
-                </div>
-
-                <div className='flex flex-col gap-2'>
-                    <div className='flex item justify-between bg-black py-[10px] px-10 rounded-lg w-full'>
-                        <p className='text-[16px] text-[#E8E1DC] font-medium'>All received proposal (ARP) </p>
-                        <p className='text-[16px] text-[#E8E1DC]'>400</p>
-                    </div>
-
-                    <div className='flex item justify-between bg-black py-[10px] px-10 rounded-lg w-full'>
-                        <p className='text-[16px] text-[#E8E1DC] font-medium'>New Development </p>
-                        <p className='text-[16px] text-[#E8E1DC]'>400</p>
-                    </div>
-                </div>
-
-                <div className='flex items-center gap-12 mt-7'>
-                    <buttton className='bg-[#FFDF00] rounded-lg py-3 px-4 cursor-pointer'>
-                        <p className='text-[20px] font-poppins text-black'>Collab Proposals</p>
-                    </buttton>
-
-                    <button className='bg-transparent border border-[#FFDF00] text-[#FFDF00] rounded-lg py-3 px-4 cursor-pointer'>
-                        <p className='text-[20px] font-poppins text-[#FFDF00] '>Collab Proposals</p>
-                    </button>
-
-                    <button className='bg-transparent border border-[#FFDF00] text-[#FFDF00] rounded-lg py-3 px-4 cursor-pointer'>
-                        <p className='text-[20px] font-poppins text-[#FFDF00] '>Collab Proposals</p>
-                    </button>
-                </div>
-            </div>
+export const DashboardListCard = () => {
+  return (
+    <div className=" py-8 px-7  flex  gap-8 bg-[#242222] rounded-xl relative">
+      <img
+        src={DashBoardImg}
+        alt="dashboard"
+        className="w-[368px] h-[300px] rounded-md border border-[#FFD21D]"
+      />
+      <img
+        src={SettingsImg}
+        alt="settings"
+        className="absolute top-8 right-8  cursor-pointer"
+      />
+      <div>
+        <div className="flex items-center gap gap-2 mb-6">
+          <h2 className="text-[39px] font-bold font-sans text-white">
+            Okay Bears
+          </h2>
+          <div className="p-2">
+            <p className="font-sans text-[13px] text-[#FFD21D] bg-[rgba(218, 165, 33, 0.05) 100%]">
+              Brand
+            </p>
+          </div>
         </div>
-    )
-}
 
-export default DashboardListCard
+        <div className="flex flex-col gap-2">
+          <div className="flex item justify-between bg-black py-[10px] px-10 rounded-lg w-full">
+            <p className="text-[16px] text-[#E8E1DC] font-medium">
+              All received proposal (ARP){" "}
+            </p>
+            <p className="text-[16px] text-[#E8E1DC]">400</p>
+          </div>
+
+          <div className="flex item justify-between bg-black py-[10px] px-10 rounded-lg w-full">
+            <p className="text-[16px] text-[#E8E1DC] font-medium">
+              New Development{" "}
+            </p>
+            <p className="text-[16px] text-[#E8E1DC]">400</p>
+          </div>
+        </div>
+
+        <div className="flex items-center gap-12 mt-7">
+          <buttton className="bg-[#FFDF00] rounded-lg py-3 px-4 cursor-pointer">
+            <p className="text-[20px] font-poppins text-black">
+              Collab Proposals
+            </p>
+          </buttton>
+
+          <button className="bg-transparent border border-[#FFDF00] text-[#FFDF00] rounded-lg py-3 px-4 cursor-pointer">
+            <p className="text-[20px] font-poppins text-[#FFDF00] ">
+              Collab Proposals
+            </p>
+          </button>
+
+          <button className="bg-transparent border border-[#FFDF00] text-[#FFDF00] rounded-lg py-3 px-4 cursor-pointer">
+            <p className="text-[20px] font-poppins text-[#FFDF00] ">
+              Collab Proposals
+            </p>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const DashboardGridCard = () => {
+  return (
+    <>
+      <div className="grid grid-cols-3 gap-4 p-9 rounded-lg m-auto bg-[#0f0f0f] border-[#242121] border-2">
+        <div className="rounded-lg border border-[#FFD21D]">
+          <img
+            src={DashBoardImg}
+            alt=""
+            className="w-[320px] h-[200px] relative rounded-t-lg"
+          />
+          <svg
+            width="32"
+            height="32"
+            viewBox="0 0 32 32"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="absolute -translate-y-48 translate-x-72 p-1.5"
+          >
+            <path
+              d="M20 16C20 18.2091 18.2092 20 16 20C13.7909 20 12 18.2091 12 16C12 13.7909 13.7909 12 16 12C18.2092 12 20 13.7909 20 16Z"
+              stroke="#F7F5DD"
+              stroke-width="2"
+            />
+            <path
+              d="M13.9246 4L13.3046 7.79859C12.1542 8.17295 11.1098 8.78044 10.2277 9.56498L6.64537 8.20265L4.57001 11.7973L7.52025 14.2095C7.39776 14.7871 7.33334 15.3861 7.33334 16C7.33334 16.6139 7.39776 17.2128 7.52024 17.7905L4.57001 20.2027L6.64537 23.7973L10.2329 22.4396C11.1139 23.2219 12.1565 23.8278 13.3046 24.2014L13.9246 28H18.0754L18.6954 24.2014C19.8458 23.8271 20.8902 23.2196 21.7723 22.435L25.3547 23.7973L27.43 20.2027L24.4798 17.7905C24.6023 17.2129 24.6667 16.6139 24.6667 16C24.6667 15.3861 24.6023 14.7872 24.4798 14.2096L27.43 11.7973L25.3547 8.20271L21.7672 9.56042C20.8862 8.77809 19.8436 8.17222 18.6954 7.79859L18.0754 4H13.9246Z"
+              stroke="#F7F5DD"
+              stroke-width="2"
+            />
+          </svg>
+          <div className="p-4 bg-[#242222] text-white font-poppins rounded-b-lg">
+            <div className="flex justify-between mb-5">
+              <h3 className="text-xl font-bold">Demitchy</h3>
+              <div>
+                <span className="text-xs text-[#FFD21D] brand">Brand</span>
+              </div>
+            </div>
+            <div className="flex justify-between border border-[#0F0F0F] rounded p-2 bg-[#0F0F0F] text-xs">
+              <span>All received proposal (ARP)</span>
+              <span>400</span>
+            </div>
+            <div className="flex justify-between border border-[#0F0F0F] rounded p-2 bg-[#0F0F0F] my-3 text-xs">
+              <span>New Development</span>
+              <span>400</span>
+            </div>
+            <button className="bg-[#FFDF00] border border-[#FFDF00] text-[#FFDF00] rounded-lg py-2 px-3 cursor-pointer w-full mt-5">
+              <p className="text-[20px] font-poppins text-black font-bold">
+                Open
+              </p>
+            </button>
+          </div>
+        </div>
+        <div className="rounded-lg border border-[#FFD21D]">
+          <img
+            src={DashBoardImg}
+            alt=""
+            className="w-[320px] h-[200px] relative rounded-t-lg"
+          />
+          <svg
+            width="32"
+            height="32"
+            viewBox="0 0 32 32"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="absolute -translate-y-48 translate-x-72 p-1.5"
+          >
+            <path
+              d="M20 16C20 18.2091 18.2092 20 16 20C13.7909 20 12 18.2091 12 16C12 13.7909 13.7909 12 16 12C18.2092 12 20 13.7909 20 16Z"
+              stroke="#F7F5DD"
+              stroke-width="2"
+            />
+            <path
+              d="M13.9246 4L13.3046 7.79859C12.1542 8.17295 11.1098 8.78044 10.2277 9.56498L6.64537 8.20265L4.57001 11.7973L7.52025 14.2095C7.39776 14.7871 7.33334 15.3861 7.33334 16C7.33334 16.6139 7.39776 17.2128 7.52024 17.7905L4.57001 20.2027L6.64537 23.7973L10.2329 22.4396C11.1139 23.2219 12.1565 23.8278 13.3046 24.2014L13.9246 28H18.0754L18.6954 24.2014C19.8458 23.8271 20.8902 23.2196 21.7723 22.435L25.3547 23.7973L27.43 20.2027L24.4798 17.7905C24.6023 17.2129 24.6667 16.6139 24.6667 16C24.6667 15.3861 24.6023 14.7872 24.4798 14.2096L27.43 11.7973L25.3547 8.20271L21.7672 9.56042C20.8862 8.77809 19.8436 8.17222 18.6954 7.79859L18.0754 4H13.9246Z"
+              stroke="#F7F5DD"
+              stroke-width="2"
+            />
+          </svg>
+          <div className="p-4 bg-[#242222] text-white font-poppins rounded-b-lg">
+            <div className="flex justify-between mb-5">
+              <h3 className="text-xl font-bold">Demitchy</h3>
+              <div>
+                <span className="text-xs text-[#FFD21D] brand">Brand</span>
+              </div>
+            </div>
+            <div className="flex justify-between border border-[#0F0F0F] rounded p-2 bg-[#0F0F0F] text-xs">
+              <span>All received proposal (ARP)</span>
+              <span>400</span>
+            </div>
+            <div className="flex justify-between border border-[#0F0F0F] rounded p-2 bg-[#0F0F0F] my-3 text-xs">
+              <span>New Development</span>
+              <span>400</span>
+            </div>
+            <button className="bg-[#FFDF00] border border-[#FFDF00] text-[#FFDF00] rounded-lg py-2 px-3 cursor-pointer w-full mt-5">
+              <p className="text-[20px] font-poppins text-black font-bold">
+                Open
+              </p>
+            </button>
+          </div>
+        </div>
+        <div className="rounded-lg border border-[#FFD21D]">
+          <img
+            src={DashBoardImg}
+            alt=""
+            className="w-[320px] h-[200px] relative rounded-t-lg"
+          />
+          <svg
+            width="32"
+            height="32"
+            viewBox="0 0 32 32"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="absolute -translate-y-48 translate-x-72 p-1.5"
+          >
+            <path
+              d="M20 16C20 18.2091 18.2092 20 16 20C13.7909 20 12 18.2091 12 16C12 13.7909 13.7909 12 16 12C18.2092 12 20 13.7909 20 16Z"
+              stroke="#F7F5DD"
+              stroke-width="2"
+            />
+            <path
+              d="M13.9246 4L13.3046 7.79859C12.1542 8.17295 11.1098 8.78044 10.2277 9.56498L6.64537 8.20265L4.57001 11.7973L7.52025 14.2095C7.39776 14.7871 7.33334 15.3861 7.33334 16C7.33334 16.6139 7.39776 17.2128 7.52024 17.7905L4.57001 20.2027L6.64537 23.7973L10.2329 22.4396C11.1139 23.2219 12.1565 23.8278 13.3046 24.2014L13.9246 28H18.0754L18.6954 24.2014C19.8458 23.8271 20.8902 23.2196 21.7723 22.435L25.3547 23.7973L27.43 20.2027L24.4798 17.7905C24.6023 17.2129 24.6667 16.6139 24.6667 16C24.6667 15.3861 24.6023 14.7872 24.4798 14.2096L27.43 11.7973L25.3547 8.20271L21.7672 9.56042C20.8862 8.77809 19.8436 8.17222 18.6954 7.79859L18.0754 4H13.9246Z"
+              stroke="#F7F5DD"
+              stroke-width="2"
+            />
+          </svg>
+          <div className="p-4 bg-[#242222] text-white font-poppins rounded-b-lg">
+            <div className="flex justify-between mb-5">
+              <h3 className="text-xl font-bold">Demitchy</h3>
+              <div>
+                <span className="text-xs text-[#FFD21D] brand">Brand</span>
+              </div>
+            </div>
+            <div className="flex justify-between border border-[#0F0F0F] rounded p-2 bg-[#0F0F0F] text-xs">
+              <span>All received proposal (ARP)</span>
+              <span>400</span>
+            </div>
+            <div className="flex justify-between border border-[#0F0F0F] rounded p-2 bg-[#0F0F0F] my-3 text-xs">
+              <span>New Development</span>
+              <span>400</span>
+            </div>
+            <button className="bg-[#FFDF00] border border-[#FFDF00] text-[#FFDF00] rounded-lg py-2 px-3 cursor-pointer w-full mt-5">
+              <p className="text-[20px] font-poppins text-black font-bold">
+                Open
+              </p>
+            </button>
+          </div>
+        </div>
+        <div className="rounded-lg border border-[#FFD21D]">
+          <img
+            src={DashBoardImg}
+            alt=""
+            className="w-[320px] h-[200px] relative rounded-t-lg"
+          />
+          <svg
+            width="32"
+            height="32"
+            viewBox="0 0 32 32"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="absolute -translate-y-48 translate-x-72 p-1.5"
+          >
+            <path
+              d="M20 16C20 18.2091 18.2092 20 16 20C13.7909 20 12 18.2091 12 16C12 13.7909 13.7909 12 16 12C18.2092 12 20 13.7909 20 16Z"
+              stroke="#F7F5DD"
+              stroke-width="2"
+            />
+            <path
+              d="M13.9246 4L13.3046 7.79859C12.1542 8.17295 11.1098 8.78044 10.2277 9.56498L6.64537 8.20265L4.57001 11.7973L7.52025 14.2095C7.39776 14.7871 7.33334 15.3861 7.33334 16C7.33334 16.6139 7.39776 17.2128 7.52024 17.7905L4.57001 20.2027L6.64537 23.7973L10.2329 22.4396C11.1139 23.2219 12.1565 23.8278 13.3046 24.2014L13.9246 28H18.0754L18.6954 24.2014C19.8458 23.8271 20.8902 23.2196 21.7723 22.435L25.3547 23.7973L27.43 20.2027L24.4798 17.7905C24.6023 17.2129 24.6667 16.6139 24.6667 16C24.6667 15.3861 24.6023 14.7872 24.4798 14.2096L27.43 11.7973L25.3547 8.20271L21.7672 9.56042C20.8862 8.77809 19.8436 8.17222 18.6954 7.79859L18.0754 4H13.9246Z"
+              stroke="#F7F5DD"
+              stroke-width="2"
+            />
+          </svg>
+          <div className="p-4 bg-[#242222] text-white font-poppins rounded-b-lg">
+            <div className="flex justify-between mb-5">
+              <h3 className="text-xl font-bold">Demitchy</h3>
+              <div>
+                <span className="text-xs text-[#FFD21D] brand">Brand</span>
+              </div>
+            </div>
+            <div className="flex justify-between border border-[#0F0F0F] rounded p-2 bg-[#0F0F0F] text-xs">
+              <span>All received proposal (ARP)</span>
+              <span>400</span>
+            </div>
+            <div className="flex justify-between border border-[#0F0F0F] rounded p-2 bg-[#0F0F0F] my-3 text-xs">
+              <span>New Development</span>
+              <span>400</span>
+            </div>
+            <button className="bg-[#FFDF00] border border-[#FFDF00] text-[#FFDF00] rounded-lg py-2 px-3 cursor-pointer w-full mt-5">
+              <p className="text-[20px] font-poppins text-black font-bold">
+                Open
+              </p>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
