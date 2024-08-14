911fc055ec7746524b0a4f9a60d34aa941428a52
Korex23 <ololokes@gmail.com>
2024-08-14 08:32:30 +0100

    testing

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -2,7 +2,7 @@ function PageTitle({ title = "", subTitle = "" }) {
   return (
     <div className="mb-12 text-center">
       <h1 className="text-4xl font-light text-white">Analytics Page</h1>
-      <p className="mt-2 text-sm text-gray-500">Check activity on your page</p>
+      <p className="mt-2 text-sm text-gray-500">Check activity on your pages</p>
     </div>
   );
 }
