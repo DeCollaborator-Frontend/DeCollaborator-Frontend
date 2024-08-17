4d563a1d689f1fe078d83493b5bd0be369b950d1
Obafemi Olorede <obafemilared@gmail.com>
2024-08-17 06:23:06 +0100

    update

--- src/pages/Chats/ChatsList.jsx
+++ src/pages/Chats/ChatsList.jsx
@@ -4,7 +4,7 @@ import Chat from "./Chat";
 const ChatsList = ({ selectedChat, onToggleSelectChat }) => {
   return (
     <div className="grid h-full  gap-4 overflow-auto pr-5">
-      {Array.from({ length: 5 }, (_, i) => (
+      {Array.from({ length: 10 }, (_, i) => (
         <Chat
           onToggleSelectChat={onToggleSelectChat}
           key={i}
